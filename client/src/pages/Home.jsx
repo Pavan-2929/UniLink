6ba47737e9f4ec43c38dbff4a163504ecde8a498
Pavan-2929 <kapadiyapavan3218@gmail.com>
2024-02-23 14:40:32 +0530

    added CRUD operation on the material

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,12 +4,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../redux/auth/authSlice";
 import { NavLink } from "react-router-dom";
 import MaterialCard from "../components/MaterialCard";
+import { FaTimes } from "react-icons/fa";
 
 const Home = () => {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
   const currentUser = useSelector((state) => state.currentUser);
+  const [showModal, setShowModal] = useState(false);
   const [userData, setUserData] = useState({});
+  const [formData, setFormData] = useState({});
+  const [materialsData, setMaterialData] = useState([]);
 
   const getUserData = async () => {
     try {
@@ -23,10 +27,46 @@ const Home = () => {
     }
   };
 
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const addMaterial = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/material/create",
+        formData
+      );
+
+      console.log(response);
+      fetchMaterials()
+      setShowModal(!showModal);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchMaterials = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/material/get"
+      );
+
+      setMaterialData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     if (isLoggedIn) {
       getUserData();
     }
+    fetchMaterials();
   }, []);
 
   return (
@@ -69,11 +109,83 @@ const Home = () => {
       )}
 
       <div className="mt-8 bg-gray-200 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-semibold pt-4">
+        <h1 className="text-2xl font-semibold pt-4 my-2">
           Find the Material for Your Curriculum
         </h1>
-        <MaterialCard />
+        {currentUser && currentUser.userType === "instructor" && (
+          <div>
+            <button
+              onClick={toggleModal}
+              className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md text-md m-4 transition duration-300"
+            >
+              Add Material
+            </button>
+          </div>
+        )}
+        {materialsData && <MaterialCard materialsData={materialsData} fetchMaterials={fetchMaterials}/>}
       </div>
+
+      {showModal && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-75">
+          <div className="bg-gray-200 md:p-10 p-4 rounded-lg shadow-lg md:w-[50vw] w-full">
+            <div className="flex justify-between items-center mb-6">
+              <h2 className="text-2xl md:text-3xl font-semibold">
+                Add new Material
+              </h2>
+              <span
+                className="cursor-pointer text-xl p-2 rounded-md bg-red-500 text-white hover:bg-red-600"
+                onClick={toggleModal}
+              >
+                <FaTimes />
+              </span>
+            </div>
+            <form onSubmit={addMaterial}>
+              <div className="mb-6">
+                <input
+                  onChange={handleChange}
+                  id="title"
+                  type="text"
+                  className="w-full px-4 py-3 border rounded-md text-lg"
+                  placeholder="Material Name"
+                />
+              </div>
+              <div className="mb-6">
+                <input
+                  id="description"
+                  onChange={handleChange}
+                  type="text"
+                  className="w-full px-4 py-3 border rounded-md text-lg"
+                  placeholder="Material Description"
+                />
+              </div>
+              <div className="mb-6">
+                <input
+                  id="materialURL"
+                  onChange={handleChange}
+                  type="text"
+                  className="w-full px-4 py-3 border rounded-md text-lg"
+                  placeholder="Material Link"
+                />
+              </div>
+              <div className="flex justify-between">
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md text-lg transition duration-300"
+                >
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  onClick={toggleModal}
+                  className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md text-lg transition duration-300"
+                >
+                  Close
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- server/routes/material.routes.js
+++ server/routes/material.routes.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { getMaterials } from '../controllers/material.controllers.js'
+import { createMaterial, deleteMaterial, getMaterial, getMaterials, updateMaterial } from '../controllers/material.controllers.js'
 
 const router = express.Router()
 
 router.get('/get', getMaterials)
+router.post('/create', createMaterial)
+router.delete('/delete/:id', deleteMaterial)
+router.put('/update/:id', updateMaterial)
+router.get('/get/:id', getMaterial)
 
 export default router
